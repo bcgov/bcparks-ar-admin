94a9cc37e84848b39c982f82c0234df577e2b11f
Daniel Truong <daniel@digitalspace.ca>
2022-03-24 23:33:16 +0000

    Adding username text to header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { ConfigService } from '../services/config.service';
+import { KeycloakService } from '../services/keycloak.service';
 
 @Component({
   selector: 'app-header',
@@ -9,8 +11,19 @@ import { ConfigService } from '../services/config.service';
 export class HeaderComponent {
   public envName: string;
   public showBanner = true;
+  public welcomeMsg: String;
+  public isAuthenticated: boolean;
 
-  constructor(protected configService: ConfigService) {
+  constructor(
+    protected configService: ConfigService,
+    protected router: Router,
+    protected keycloakService: KeycloakService
+  ) {
+    router.events.subscribe(() => {
+      this.isAuthenticated = this.keycloakService.isAuthenticated();
+      this.welcomeMsg = this.keycloakService.getWelcomeMessage();
+    });
+
     this.envName = this.configService.config['ENVIRONMENT'];
     if (this.envName === 'prod') {
       this.showBanner = false;
