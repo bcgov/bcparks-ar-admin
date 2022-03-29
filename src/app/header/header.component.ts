96cb70b31490e6890e56939ed81df5fd6219eaf4
Daniel Truong <daniel@digitalspace.ca>
2022-03-29 20:32:40 +0000

    Setting up mobile nav

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnDestroy } from '@angular/core';
+import { NavigationEnd, Router, Event } from '@angular/router';
+import { filter, takeWhile } from 'rxjs';
 import { ConfigService } from '../services/config.service';
 import { KeycloakService } from '../services/keycloak.service';
 
@@ -8,25 +9,49 @@ import { KeycloakService } from '../services/keycloak.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnDestroy {
+  private subscriptions: any[] = [];
+  private alive = true;
+
   public envName: string;
   public showBanner = true;
   public welcomeMsg: String;
   public isAuthenticated: boolean;
+  public isMenuCollapsed = true;
+  public routes: any[] = [];
+  public currentRoute: any;
 
   constructor(
     protected configService: ConfigService,
     protected router: Router,
     protected keycloakService: KeycloakService
   ) {
-    router.events.subscribe(() => {
-      this.isAuthenticated = this.keycloakService.isAuthenticated();
-      this.welcomeMsg = this.keycloakService.getWelcomeMessage();
+    this.routes = router.config.filter(function (obj) {
+      return obj.path !== '**' && obj.path !== 'unauthorized';
     });
+
+    this.subscriptions.push(
+      router.events
+        .pipe(filter((event) => event instanceof NavigationEnd))
+        .pipe(takeWhile(() => this.alive))
+        .subscribe((event: Event) => {
+          this.currentRoute = event;
+        })
+    );
+
+    this.isAuthenticated = this.keycloakService.isAuthenticated();
+    this.welcomeMsg = this.keycloakService.getWelcomeMessage();
 
     this.envName = this.configService.config['ENVIRONMENT'];
     if (this.envName === 'prod') {
       this.showBanner = false;
+    }
+  }
+
+  ngOnDestroy() {
+    this.alive = false;
+    for (let i = 0; i < this.subscriptions.length; i++) {
+      this.subscriptions[i].unsubscribe();
     }
   }
 }
--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 import { KeycloakService } from '../services/keycloak.service';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, NgbModule],
   exports: [HeaderComponent],
   providers: [KeycloakService],
 })
