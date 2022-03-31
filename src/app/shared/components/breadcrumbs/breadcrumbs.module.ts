480a7e2a683397c276ba91bf60e9e1a73806a280
Daniel Truong <daniel@digitalspace.ca>
2022-03-31 18:38:34 +0000

    Fixing routing issue

--- src/app/shared/components/breadcrumbs/breadcrumbs.module.ts
+++ src/app/shared/components/breadcrumbs/breadcrumbs.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BreadcrumbsComponent } from './breadcrumbs.component';
-import { RouterTestingModule } from '@angular/router/testing';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 @NgModule({
   declarations: [BreadcrumbsComponent],
-  imports: [CommonModule, RouterTestingModule],
+  imports: [CommonModule, AppRoutingModule],
   exports: [BreadcrumbsComponent],
 })
 export class BreadcrumbsModule {}
