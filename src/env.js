af9457ffe6b05f8aa9e3ecd181a2cbca35a999f9
Mark Lisé <marklise@users.noreply.github.com>
2022-04-05 09:11:14 -0700

    Update env.js
    Switch to dev environment.

--- src/env.js
+++ src/env.js
@@ -18,7 +18,7 @@
 
     // Keycloak
     window.__env.KEYCLOAK_CLIENT_ID = 'parking-pass';
-    window.__env.KEYCLOAK_URL = 'https://oidc.gov.bc.ca/auth';
+    window.__env.KEYCLOAK_URL = 'https://dev.oidc.gov.bc.ca/auth';
     window.__env.KEYCLOAK_REALM = 'g7v0xlf4';
     window.__env.KEYCLOAK_ENABLED = true;
 }(this));
