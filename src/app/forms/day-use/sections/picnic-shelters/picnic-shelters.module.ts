eddb6e8082ca520cdf5f770afe88b48420bbffee
Meredith <meredith@digitalspace.ca>
2022-04-14 23:49:11 +0000

    BRS-536: Day use form UI, small fix in base.scss

--- src/app/forms/day-use/sections/picnic-shelters/picnic-shelters.module.ts
+++ src/app/forms/day-use/sections/picnic-shelters/picnic-shelters.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PicnicSheltersComponent } from './picnic-shelters.component';
+import { TextInputModule } from 'src/app/shared/components/forms/text-input/text-input.module';
+import { CalculationDisplayModule } from 'src/app/shared/components/forms/calculation-display/calculation-display.module';
 
 @NgModule({
   declarations: [PicnicSheltersComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, TextInputModule, CalculationDisplayModule],
   exports: [PicnicSheltersComponent],
 })
 export class PicnicSheltersModule {}
