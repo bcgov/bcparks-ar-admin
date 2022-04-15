f8e20230fd5e423d9b56e82f66d3dcab50e9378f
Meredith <meredith@digitalspace.ca>
2022-04-15 01:22:48 +0000

    BRS-535: group camping form created

--- src/app/forms/group-camping/sections/youth-rate-groups/youth-rate-groups.module.ts
+++ src/app/forms/group-camping/sections/youth-rate-groups/youth-rate-groups.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { YouthRateGroupsComponent } from './youth-rate-groups.component';
+import { InfoTextModule } from 'src/app/shared/components/info-text/info-text.module';
+import { TextInputModule } from 'src/app/shared/components/forms/text-input/text-input.module';
+import { CalculationDisplayModule } from 'src/app/shared/components/forms/calculation-display/calculation-display.module';
 
 @NgModule({
   declarations: [YouthRateGroupsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, InfoTextModule, TextInputModule, CalculationDisplayModule],
   exports: [YouthRateGroupsComponent],
 })
 export class YouthRateGroupsModule {}
