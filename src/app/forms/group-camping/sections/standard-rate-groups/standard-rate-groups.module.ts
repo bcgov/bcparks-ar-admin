f171f2f0543686680aa6f4650a910470b9108783
meredithom <meredith@digitalspace.ca>
2022-04-14 18:25:26 -0700

    BRS-535: group camping form created (#36)

--- src/app/forms/group-camping/sections/standard-rate-groups/standard-rate-groups.module.ts
+++ src/app/forms/group-camping/sections/standard-rate-groups/standard-rate-groups.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StandardRateGroupsComponent } from './standard-rate-groups.component';
+import { TextInputModule } from 'src/app/shared/components/forms/text-input/text-input.module';
+import { CalculationDisplayModule } from 'src/app/shared/components/forms/calculation-display/calculation-display.module';
 
 @NgModule({
   declarations: [StandardRateGroupsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, TextInputModule, CalculationDisplayModule],
   exports: [StandardRateGroupsComponent],
 })
 export class StandardRateGroupsModule {}
