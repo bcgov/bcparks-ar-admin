70cc2bd552ab89366ac2c84383cf24c6878dce85
Meredith <meredith@digitalspace.ca>
2022-04-15 00:55:58 +0000

    BRS-495: Frontcountry camping form UI

--- src/app/forms/frontcountry-camping/sections/additional-vehicles/additional-vehicles.module.ts
+++ src/app/forms/frontcountry-camping/sections/additional-vehicles/additional-vehicles.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdditionalVehiclesComponent } from './additional-vehicles.component';
+import { InfoTextModule } from 'src/app/shared/components/info-text/info-text.module';
+import { TextInputModule } from 'src/app/shared/components/forms/text-input/text-input.module';
+import { CalculationDisplayModule } from 'src/app/shared/components/forms/calculation-display/calculation-display.module';
 
 @NgModule({
   declarations: [AdditionalVehiclesComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, InfoTextModule, TextInputModule, CalculationDisplayModule],
   exports: [AdditionalVehiclesComponent],
 })
 export class AdditionalVehiclesModule {}
