49619c7a67641e1c91c4c8f9a4cddb19a926828f
meredithom <meredith@digitalspace.ca>
2022-04-14 18:00:46 -0700

    BRS-495: Frontcountry camping form UI (#35)

--- src/app/forms/frontcountry-camping/sections/camping-party-nights/camping-party-nights.module.ts
+++ src/app/forms/frontcountry-camping/sections/camping-party-nights/camping-party-nights.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CampingPartyNightsComponent } from './camping-party-nights.component';
+import { InfoTextModule } from 'src/app/shared/components/info-text/info-text.module';
+import { TextInputModule } from 'src/app/shared/components/forms/text-input/text-input.module';
+import { CalculationDisplayModule } from 'src/app/shared/components/forms/calculation-display/calculation-display.module';
 
 @NgModule({
   declarations: [CampingPartyNightsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, InfoTextModule, TextInputModule, CalculationDisplayModule],
   exports: [CampingPartyNightsComponent],
 })
 export class CampingPartyNightsModule {}
--- src/app/forms/frontcountry-camping/sections/other-frontcountry-camping-revenue/other-frontcountry-camping-revenue.module.ts
+++ src/app/forms/frontcountry-camping/sections/other-frontcountry-camping-revenue/other-frontcountry-camping-revenue.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OtherFrontcountryCampingRevenueComponent } from './other-frontcountry-camping-revenue.component';
+import { InfoTextModule } from 'src/app/shared/components/info-text/info-text.module';
+import { TextInputModule } from 'src/app/shared/components/forms/text-input/text-input.module';
+import { CalculationDisplayModule } from 'src/app/shared/components/forms/calculation-display/calculation-display.module';
 
 @NgModule({
   declarations: [OtherFrontcountryCampingRevenueComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, InfoTextModule, TextInputModule, CalculationDisplayModule],
   exports: [OtherFrontcountryCampingRevenueComponent],
 })
 export class OtherFrontcountryCampingRevenueModule {}
