d4028c69fbbb1c6daf8f18bad2302cade0800cdd
meredithom <meredith@digitalspace.ca>
2022-04-14 17:29:06 -0700

    BRS-536: Day use form UI, small fix in base.scss (#32)

--- src/app/forms/day-use/sections/people-and-vehicles/people-and-vehicles.module.ts
+++ src/app/forms/day-use/sections/people-and-vehicles/people-and-vehicles.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PeopleAndVehiclesComponent } from './people-and-vehicles.component';
+import { InfoTextModule } from 'src/app/shared/components/info-text/info-text.module';
+import { TextInputModule } from 'src/app/shared/components/forms/text-input/text-input.module';
+import { CalculationDisplayModule } from 'src/app/shared/components/forms/calculation-display/calculation-display.module';
 
 @NgModule({
   declarations: [PeopleAndVehiclesComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, InfoTextModule, TextInputModule, CalculationDisplayModule],
   exports: [PeopleAndVehiclesComponent],
 })
 export class PeopleAndVehiclesModule {}
