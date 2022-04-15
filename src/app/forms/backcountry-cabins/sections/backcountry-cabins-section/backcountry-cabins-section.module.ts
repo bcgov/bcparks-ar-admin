614a13b2a45b28d5bb544366afbdb3e6746c039d
Meredith <meredith@digitalspace.ca>
2022-04-15 01:39:59 +0000

    BRS-538: backcountry cabins form created

--- src/app/forms/backcountry-cabins/sections/backcountry-cabins-section/backcountry-cabins-section.module.ts
+++ src/app/forms/backcountry-cabins/sections/backcountry-cabins-section/backcountry-cabins-section.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BackcountryCabinsSectionComponent } from './backcountry-cabins-section.component';
+import { TextInputModule } from 'src/app/shared/components/forms/text-input/text-input.module';
+import { CalculationDisplayModule } from 'src/app/shared/components/forms/calculation-display/calculation-display.module';
 
 @NgModule({
   declarations: [BackcountryCabinsSectionComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, TextInputModule, CalculationDisplayModule],
   exports: [BackcountryCabinsSectionComponent],
 })
 export class BackcountryCabinsSectionModule {}
