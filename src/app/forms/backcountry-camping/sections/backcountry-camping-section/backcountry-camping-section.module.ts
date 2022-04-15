7a30aa8e97129af73e8dbf51c1973b022a75deb2
meredithom <meredith@digitalspace.ca>
2022-04-14 18:40:53 -0700

    BRS-537: backcountry camping from created (#37)

--- src/app/forms/backcountry-camping/sections/backcountry-camping-section/backcountry-camping-section.module.ts
+++ src/app/forms/backcountry-camping/sections/backcountry-camping-section/backcountry-camping-section.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BackcountryCampingSectionComponent } from './backcountry-camping-section.component';
+import { InfoTextModule } from 'src/app/shared/components/info-text/info-text.module';
+import { TextInputModule } from 'src/app/shared/components/forms/text-input/text-input.module';
+import { CalculationDisplayModule } from 'src/app/shared/components/forms/calculation-display/calculation-display.module';
 
 
 
 @NgModule({
-  declarations: [
-    BackcountryCampingSectionComponent
-  ],
-  imports: [
-    CommonModule
-  ],
+  declarations: [BackcountryCampingSectionComponent],
+  imports: [CommonModule, InfoTextModule, TextInputModule, CalculationDisplayModule],
   exports:[BackcountryCampingSectionComponent]
 })
 export class BackcountryCampingSectionModule { }
