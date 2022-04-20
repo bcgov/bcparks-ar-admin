9295850fd1a11c18aaaa27da0e4f3a50ab04b6d6
Daniel Truong <daniel@digitalspace.ca>
2022-04-20 00:30:03 +0000

    NOBUG: Creating for service for posting activity

---
+++ src/app/services/form.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FormService {
+  constructor(public apiService: ApiService) {}
+
+  async postActivity(obj) {
+    let res;
+    try {
+      // We require queryParam type = activity
+      // In obj we need orcs, subAreaName, activity and date
+      if (obj.orcs && obj.subAreaName && obj.activity && obj.date) {
+        res = await this.apiService.post('subarea', obj, { type: 'activity' });
+      }
+    } catch (e) {
+      // TODO: Deal with error here
+    }
+  }
+}
