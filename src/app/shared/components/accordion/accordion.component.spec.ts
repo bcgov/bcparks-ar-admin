72a6566361c3822e3bd88c7b22946fb188f48f54
meredithom <meredith@digitalspace.ca>
2022-05-03 14:27:45 -0700

    BRS-556: updated UI for accordion components to match ticket BRS-556. Clean-up of unused imports in accordion components. (#119)

--- src/app/shared/components/accordion/accordion.component.spec.ts
+++ src/app/shared/components/accordion/accordion.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AccordionComponent } from './accordion.component';
