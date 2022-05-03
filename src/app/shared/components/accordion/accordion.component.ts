e44f063c014cb32f36360fb1dac405a308791321
Meredith <meredith@digitalspace.ca>
2022-05-03 21:20:48 +0000

    BRS-556: updated UI for accordion components to match ticket BRS-556. Clean-up of unused imports in accordion components.

--- src/app/shared/components/accordion/accordion.component.ts
+++ src/app/shared/components/accordion/accordion.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription, takeWhile } from 'rxjs';
 import { DataService } from 'src/app/services/data.service';
