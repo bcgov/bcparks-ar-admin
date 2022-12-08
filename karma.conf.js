d068196db150eba8803f194de371dbcb0ae2da20
Mark Lisé <marklise@users.noreply.github.com>
2022-12-08 15:10:57 -0800

    NOBUG: Enable code coverage on cli. (#182)

--- karma.conf.js
+++ karma.conf.js
@@ -28,8 +28,7 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, './coverage/bcparks-ar-admin'),
       subdir: '.',
       reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'text' }
       ]
     },
     reporters: ['progress', 'kjhtml'],
