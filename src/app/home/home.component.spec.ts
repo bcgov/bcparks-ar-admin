11108aed602c406138085f7375bbc15e11710902
Mark Lisé <mark@digitalspace.ca>
2022-12-22 16:35:16 -0800

    BRS-966: Increase code coverage of home component.

---
+++ src/app/home/home.component.spec.ts
@@ -0,0 +1,39 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ConfigService } from '../services/config.service';
+import { KeycloakService } from '../services/keycloak.service';
+import { HomeComponent } from './home.component';
+
+describe('HomeComponent', () => {
+  const mockKeycloakService = jasmine.createSpyObj('KeycloakService', [
+    'isAllowed'
+  ]);
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HomeComponent],
+      providers: [
+        ConfigService,
+        { provide: KeycloakService, useValue: mockKeycloakService }],
+    }).compileComponents();
+  });
+
+  it('should create and have one card', () => {
+    mockKeycloakService.isAllowed.and.returnValue(false);
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+    expect(component.cardConfig.length).toBe(1);
+  });
+
+    it('should create and have three cards', () => {
+    mockKeycloakService.isAllowed.and.returnValue(true);
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    expect(component).toBeTruthy();
+    expect(component.cardConfig.length).toBe(3);
+  });
+});
