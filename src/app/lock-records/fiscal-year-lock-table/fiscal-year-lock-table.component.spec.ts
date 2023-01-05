399e7064590244dca41e8cfd02535bf21b1d4085
Mark Lisé <marklise@users.noreply.github.com>
2023-01-05 09:41:52 -0800

    BRS-966: Adding ut coverage 100% to fiscal year lock table. (#194)
    * BRS-966: Adding ut coverage 100% to fiscal year lock table.
    
    * Remove unused elements.
    
    * Fixing toBe=>toEqual

--- src/app/lock-records/fiscal-year-lock-table/fiscal-year-lock-table.component.spec.ts
+++ src/app/lock-records/fiscal-year-lock-table/fiscal-year-lock-table.component.spec.ts
@@ -1,24 +1,51 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs/internal/observable/of';
+import { DataService } from '../../services/data.service';
 import { FiscalYearLockTableComponent } from './fiscal-year-lock-table.component';
 
 describe('FiscalYearLockTableComponent', () => {
   let component: FiscalYearLockTableComponent;
   let fixture: ComponentFixture<FiscalYearLockTableComponent>;
+  let dataService: DataService;
+
+  const mockDataService = {
+    getItemValue: () => {
+      return of([
+        {
+            isLocked: true
+        }])
+    }
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [FiscalYearLockTableComponent],
+      providers: [
+        {
+          provide: DataService, useValue: mockDataService
+        }
+      ]
     }).compileComponents();
+    dataService = TestBed.inject(DataService);
   });
 
-  beforeEach(() => {
+  it('should test all elements of the table component', async () => {
     fixture = TestBed.createComponent(FiscalYearLockTableComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.tableRows.length).toBe(1);
+
+    await component.ngOnInit();
+    await fixture.isStable();
+
+    expect(component.columnSchema.length).toBe(3);
+
+    expect(component.getFiscalYearString('2012')).toEqual('2011–12');
+    expect(component.columnSchema[0].mapValue({ sk: '2012', isLocked: true })).toBe('2012');
+    expect(component.columnSchema[0].mapDisplay?.({ sk: '2012', isLocked: true })).toBe('2011–12');
+    expect(component.columnSchema[1].mapValue()).toBe('All Parks');
+    expect(component.columnSchema[2].mapValue({ sk: '2012', isLocked: true })).toBe(true);
+    expect(component.columnSchema[2].mapValue({ sk: '2012', isLocked: false })).toBe(false);
+    expect(component.columnSchema[2].cellTemplate?.({ sk: '2012', isLocked: false })?.data?.data).toEqual({ sk: '2012', isLocked: false });
   });
 });
