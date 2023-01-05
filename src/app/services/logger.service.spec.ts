898d21523d9b1f6a06b5e43c214df64e4121c705
Mark Lisé <mark@digitalspace.ca>
2023-01-05 09:48:37 -0800

    BRS-966: Adding up to 80% CC for logger.

--- src/app/services/logger.service.spec.ts
+++ src/app/services/logger.service.spec.ts
@@ -1,20 +1,31 @@
 import { HttpClient, HttpHandler } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { ConfigService } from './config.service';
-
 import { LoggerService } from './logger.service';
 
 describe('LoggerService', () => {
-  let service: LoggerService;
+  let loggerService: LoggerService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [ ConfigService, HttpClient, HttpHandler ]
     });
-    service = TestBed.inject(LoggerService);
+    loggerService = TestBed.inject(LoggerService);
+    spyOn(LoggerService.prototype, "log").and.callThrough();
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(loggerService).toBeTruthy();
+    loggerService.debug('Some Debug Message');
+    expect(LoggerService.prototype.log).toHaveBeenCalledTimes(1);
+
+    loggerService.info('Some Info Message');
+    expect(LoggerService.prototype.log).toHaveBeenCalledTimes(2);
+
+    loggerService.warn('Some Warn Message');
+    expect(LoggerService.prototype.log).toHaveBeenCalledTimes(3);
+
+    loggerService.fatal('Some Fatal Message');
+    expect(LoggerService.prototype.log).toHaveBeenCalledTimes(4);
   });
 });
