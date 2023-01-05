a9a014e09d8a6334cfd0185cf1d100f8904f5892
Mark Lisé <marklise@users.noreply.github.com>
2023-01-05 15:26:14 -0800

    BRS-966: Adding CC 100% to loading service. (#197)

--- src/app/services/loading.service.spec.ts
+++ src/app/services/loading.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { Constants } from '../shared/utils/constants';
 
 import { LoadingService } from './loading.service';
 
@@ -10,7 +11,45 @@ describe('LoadingService', () => {
     service = TestBed.inject(LoadingService);
   });
 
-  it('should be created', () => {
+  it('should be created', async () => {
     expect(service).toBeTruthy();
+
+    let subArray: any[] = [];
+    let subLoadingArray: any[] = [];
+    subLoadingArray.push(false);
+    subLoadingArray.push(true);
+    subLoadingArray.push(false);
+
+    // Need to push the first sub in there
+    subArray.push({});
+
+    const obj = {
+      'enterDataPark': {
+        'loading': true
+      }
+    };
+    subArray.push(obj);
+
+    const obj2 = {};
+    subArray.push(obj2);
+
+    const sub = service.getFetchList().subscribe(
+      async (obs) => {
+        const poppedItem = subArray.pop();
+        expect(obs).toEqual(poppedItem);
+      }
+    );
+    const subLoading = service.getLoadingStatus().subscribe(
+      async (obsLoading) => {
+        const poppedItem = subLoadingArray.pop();
+        expect(obsLoading).toEqual(poppedItem);
+      }
+    );
+    await service.addToFetchList(Constants.dataIds.ENTER_DATA_PARK);
+    await service.removeToFetchList(Constants.dataIds.ENTER_DATA_PARK);
+
+    sub.unsubscribe();
+    subLoading.unsubscribe();
   });
+
 });
