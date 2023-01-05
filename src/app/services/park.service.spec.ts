f5d54eaae07e907bc35f7200d8b1104c825d9e99
Mark Lisé <marklise@users.noreply.github.com>
2023-01-05 12:44:02 -0800

    BRS-966: Adding 100% CC for park service. (#196)

--- src/app/services/park.service.spec.ts
+++ src/app/services/park.service.spec.ts
@@ -4,12 +4,37 @@ import { ConfigService } from './config.service';
 import { DataService } from './data.service';
 import { EventService } from './event.service';
 import { LoadingService } from './loading.service';
+import { Constants } from '../shared/utils/constants';
 
 import { ParkService } from './park.service';
 import { ToastService } from './toast.service';
+import { ApiService } from './api.service';
+import { of } from 'rxjs';
 
 describe('ParkService', () => {
   let service: ParkService;
+  let loadingServiceSpy;
+  let dataServiceSpy;
+
+  let mockApiService = {
+    get: () => {
+      return of({
+        key: 'value'
+      })
+    }
+  }
+
+  let mockApiServiceNoData = {
+    get: () => {
+      return of(null)
+    }
+  }
+
+  let mockToastService = {
+    addMessage: () => {
+      // stub
+    }
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -17,16 +42,44 @@ describe('ParkService', () => {
         HttpClient,
         DataService,
         EventService,
-        ToastService,
+        {
+          provide: ToastService, useValue: mockToastService
+        },
+        {
+          provide: ApiService, useValue: mockApiService
+        },
         HttpHandler,
         ConfigService,
         LoadingService,
       ],
     });
-    service = TestBed.inject(ParkService);
   });
 
-  it('should be created', () => {
+  it('should be created and set item values', async () => {
+    service = TestBed.inject(ParkService);
+    dataServiceSpy = spyOn(service['dataService'], 'setItemValue');
+    loadingServiceSpy = spyOn(service['loadingService'], 'addToFetchList');
+    let loggerServiceDebugSpy = spyOn(service['loggerService'], 'debug');
+
     expect(service).toBeTruthy();
+    await service.fetchEnterDataPark();
+
+    expect(loadingServiceSpy).toHaveBeenCalledWith(Constants.dataIds.ENTER_DATA_PARK);
+    expect(loggerServiceDebugSpy).toHaveBeenCalledTimes(1);
+    expect(dataServiceSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('it throws an error inside the fetch function', async () => {
+    TestBed.overrideProvider(ApiService, { useValue: mockApiServiceNoData });
+    service = TestBed.inject(ParkService);
+    loadingServiceSpy = spyOn(service['loadingService'], 'addToFetchList');
+    let loggerServiceSpy = spyOn(service['loggerService'], 'error');
+    let toastServiceSpy = spyOn(service['toastService'], 'addMessage');
+
+    expect(service).toBeTruthy();
+    await service.fetchEnterDataPark();
+
+    expect(loggerServiceSpy).toHaveBeenCalledTimes(1);
+    expect(toastServiceSpy).toHaveBeenCalledTimes(1);
   });
 });
