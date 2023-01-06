6866ea6a74bb7cb9c81ef5de2214acbd010f153f
Mark Lisé <marklise@users.noreply.github.com>
2023-01-06 09:36:33 -0800

    BRS-966: Adding CC to export and config service. (#198)

--- src/app/services/config.service.spec.ts
+++ src/app/services/config.service.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHandler } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
 import { ConfigService } from './config.service';
 import { LoggerService } from './logger.service';
@@ -7,14 +8,105 @@ import { LoggerService } from './logger.service';
 describe('ConfigService', () => {
   let service: ConfigService;
 
+  let mockHttpClient = {
+    get: (location) => {
+      console.log("Getting configuration:", location);
+      return of({
+        debug: true,
+        configurationEndpoint: true
+      })
+    }
+  }
+
+  let mockHttpClientLogLevelZero = {
+    get: (location) => {
+      console.log("Getting configuration:", location);
+      return of({
+        debug: true,
+        configurationEndpoint: true,
+        logLevel: 0
+      })
+    }
+  }
+
+  let mockHttpClientFailedThrow = {
+    get: (location) => {
+      throw 'woops'
+    }
+  }
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HttpClient, LoggerService, ConfigService, HttpHandler],
+      providers: [
+        {
+          provide: HttpClient, useValue: mockHttpClient
+        },
+        LoggerService,
+        ConfigService,
+        HttpHandler
+      ],
     });
-    service = TestBed.inject(ConfigService);
   });
 
-  it('should be created', () => {
+  it('should be created', async () => {
+    window['__env'] = {
+      debug: true,
+      configEndpoint: true,
+      logLevel: 0
+    }
+    service = TestBed.inject(ConfigService);
     expect(service).toBeTruthy();
+    expect(service.config).toEqual({});
+  });
+
+  it('should be created and log level 0 without throw', async () => {
+    window['__env'] = {
+      debug: true,
+      configEndpoint: true
+    }
+    TestBed.overrideProvider(HttpClient, { useValue: mockHttpClientLogLevelZero });
+    service = TestBed.inject(ConfigService);
+    expect(service).toBeTruthy();
+
+    expect(service.config).toEqual({});
+    expect(service.logLevel).toEqual(undefined);
+    const consoleError = spyOn(console, 'error');
+
+    await service.init();
+
+    expect(consoleError).toHaveBeenCalledTimes(0);
+  });
+
+  it('should be created and have log level 0', async () => {
+    window['__env'] = {
+      debug: true,
+      configEndpoint: true,
+      logLevel: 0
+    }
+    TestBed.overrideProvider(HttpClient, { useValue: mockHttpClientLogLevelZero });
+    service = TestBed.inject(ConfigService);
+
+    expect(service).toBeTruthy();
+    await service.init();
+    expect(service.logLevel).toEqual(0);
+  });
+
+  it('should be created and throw', async () => {
+    window['__env'] = {
+      debug: true,
+      configEndpoint: true,
+      logLevel: 3
+    }
+    TestBed.overrideProvider(HttpClient, { useValue: mockHttpClientFailedThrow });
+    service = TestBed.inject(ConfigService);
+
+    const consoleError = spyOn(console, 'error');
+
+    expect(service).toBeTruthy();
+    expect(service.config).toEqual({});
+
+    await service.init();
+
+    expect(consoleError).toHaveBeenCalled();
   });
 });
--- src/app/services/export.service.spec.ts
+++ src/app/services/export.service.spec.ts
@@ -1,20 +1,88 @@
 import { HttpClient, HttpHandler } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { ConfigService } from './config.service';
+import { Constants } from '../shared/utils/constants';
+import { ApiService } from './api.service';
 
+import { of } from 'rxjs';
 import { ExportService } from './export.service';
 
 describe('ExportService', () => {
   let service: ExportService;
+  let dataServiceSpy;
+
+  let mockApiService = {
+    get: () => {
+      return of({
+        key: 'value'
+      })
+    }
+  }
+
+  let mockApiServiceNoData = {
+    get: () => {
+      return of(null)
+    }
+  }
+
+  let mockApiServiceNoDataThrow = {
+    get: () => {
+      throw 'Error'
+    }
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HttpClient, HttpHandler, ConfigService],
+      providers: [
+        HttpClient,
+        HttpHandler,
+        ConfigService,
+        {
+          provide: ApiService, useValue: mockApiService
+        }
+      ],
     });
-    service = TestBed.inject(ExportService);
   });
 
-  it('should be created', () => {
+  it('should be created and check for reports', async () => {
+    service = TestBed.inject(ExportService);
     expect(service).toBeTruthy();
+    dataServiceSpy = spyOn(service['dataService'], 'setItemValue');
+
+    await service.checkForReports(Constants.dataIds.EXPORT_ALL_POLLING_DATA);
+    await service.checkForReports(Constants.dataIds.EXPORT_ALL_POLLING_DATA, {
+      key: 'value'
+    });
+
+    expect(dataServiceSpy).toHaveBeenCalledTimes(2);
+  });
+
+  it('should be created and error on checkForReports', async () => {
+    TestBed.overrideProvider(ApiService, { useValue: mockApiServiceNoData });
+    service = TestBed.inject(ExportService);
+    dataServiceSpy = spyOn(service['dataService'], 'setItemValue');
+    await service.checkForReports(Constants.dataIds.EXPORT_ALL_POLLING_DATA, {
+      key: 'value'
+    });
+
+    expect(dataServiceSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should generateReport', async () => {
+    TestBed.overrideProvider(ApiService, { useValue: mockApiService });
+    service = TestBed.inject(ExportService);
+    dataServiceSpy = spyOn(service['dataService'], 'setItemValue');
+    await service.generateReport(Constants.dataIds.EXPORT_ALL_POLLING_DATA);
+
+    expect(dataServiceSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should generateReport and error', async () => {
+    TestBed.overrideProvider(ApiService, { useValue: mockApiServiceNoDataThrow });
+    service = TestBed.inject(ExportService);
+    let loggerServiceSpy = spyOn(service['loggerService'], 'error');
+    await service.generateReport(Constants.dataIds.EXPORT_ALL_POLLING_DATA);
+
+    expect(loggerServiceSpy).toHaveBeenCalledTimes(1);
   });
 });
