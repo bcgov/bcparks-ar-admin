311f3569af7eaf3fa834a8e41a045d510d6db7b4
Mark Lisé <mark@digitalspace.ca>
2023-01-06 09:01:59 -0800

    BRS-966: CC for api service.

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -8,14 +8,49 @@ import { LoggerService } from './logger.service';
 describe('ApiService', () => {
   let service: ApiService;
 
+  let mockConfigService = {
+    config: {
+      'API_LOCATION': '/here',
+      'API_PUBLIC_PATH': '/there',
+      'ENVIRONMENT': 'test'
+    }
+  }
+
+  let mockHttpClient = {
+    post: () => {
+      return {};
+    }
+  }
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HttpClient, HttpHandler, ConfigService, LoggerService],
+      providers: [
+        {
+          provide: HttpClient, useValue: mockHttpClient
+        },
+        HttpHandler,
+        {
+          provide: ConfigService, useValue: mockConfigService
+        },
+        LoggerService
+      ],
     });
     service = TestBed.inject(ApiService);
   });
 
-  it('should be created', () => {
+  it('should be created and setup apiPath', async () => {
     expect(service).toBeTruthy();
+
+    const env = service.getEnvironment() as any;
+    expect(env).toBe(undefined);
+
+    await service.init();
+
+    expect(service.apiPath).toEqual('/here/there');
+
+    const postResponse = await service.post('pk', 'sk') as any;
+    const getResponse = await service.get('pk') as any;
+    expect(postResponse).toEqual({});
+    expect(getResponse).toEqual({});
   });
 });
