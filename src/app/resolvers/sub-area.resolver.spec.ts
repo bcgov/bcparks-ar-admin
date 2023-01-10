f56f99d7d7c67604710f40795cf761883cec68f4
Mark Lisé <mark@digitalspace.ca>
2023-01-09 16:51:19 -0800

    BRS-966: Adding code coverage to sub-area resolver.

--- src/app/resolvers/sub-area.resolver.spec.ts
+++ src/app/resolvers/sub-area.resolver.spec.ts
@@ -1,20 +1,89 @@
 import { HttpClient, HttpHandler } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { ActivatedRouteSnapshot, Router } from '@angular/router';
 import { ConfigService } from '../services/config.service';
+import { convertToParamMap } from '@angular/router';
 
 import { SubAreaResolver } from './sub-area.resolver';
+import { SubAreaService } from '../services/sub-area.service';
 
 describe('SubAreaResolver', () => {
   let resolver: SubAreaResolver;
+  let router;
+
+  let params = {
+    date: new Date(),
+    orcs: '0001',
+    subAreaId: '0001',
+  }
+
+  let paramsMissingData = {
+    date: new Date()
+  }
+
+  const route: ActivatedRouteSnapshot = {
+    queryParams: params,
+    url: [],
+    params: {},
+    fragment: null,
+    data: {},
+    outlet: '',
+    component: null,
+    routeConfig: null,
+    title: undefined,
+    root: new ActivatedRouteSnapshot,
+    parent: null,
+    firstChild: null,
+    children: [],
+    pathFromRoot: [],
+    paramMap: convertToParamMap(params),
+    queryParamMap: convertToParamMap(params)
+  }
+
+  const routeMissingData: ActivatedRouteSnapshot = {
+    queryParams: paramsMissingData,
+    url: [],
+    params: {},
+    fragment: null,
+    data: {},
+    outlet: '',
+    component: null,
+    routeConfig: null,
+    title: undefined,
+    root: new ActivatedRouteSnapshot,
+    parent: null,
+    firstChild: null,
+    children: [],
+    pathFromRoot: [],
+    paramMap: convertToParamMap(paramsMissingData),
+    queryParamMap: convertToParamMap(paramsMissingData)
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [HttpClient, HttpHandler, ConfigService],
+      providers: [
+        HttpClient,
+        HttpHandler,
+        ConfigService
+      ],
     });
+    router = TestBed.inject(Router);
     resolver = TestBed.inject(SubAreaResolver);
   });
 
-  it('should be created', () => {
+  it('should be created and call fetchSubArea', async () => {
+    const serviceSpy = spyOn(SubAreaService.prototype, "fetchSubArea")
+      .and.returnValue(Promise.resolve({}) as any);
     expect(resolver).toBeTruthy();
+    await resolver.resolve(route);
+    expect(serviceSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should be created and not fetch data', async () => {
+    const serviceSpy = spyOn(SubAreaService.prototype, "fetchSubArea")
+      .and.returnValue(Promise.resolve({}) as any);
+    expect(resolver).toBeTruthy();
+    await resolver.resolve(route);
+    expect(serviceSpy).toHaveBeenCalledTimes(1);
   });
 });
