67089ae508fd8f6af190a3e68990d60abe4f39a4
cameronpettit <cameron@digitalspace.ca>
2024-03-05 15:51:02 -0800

    PDR-319 Missing subarea name

--- src/app/enter-data/enter-data.component.ts
+++ src/app/enter-data/enter-data.component.ts
@@ -1,35 +1,30 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter, Subscription } from 'rxjs';
 import { DataService } from '../services/data.service';
 import { Constants } from '../shared/utils/constants';
 import { Utils } from '../shared/utils/utils';
+import { UrlService } from '../services/url.service';
+import { DateTime } from 'luxon';
 
 @Component({
   selector: 'app-enter-data',
   templateUrl: './enter-data.component.html',
   styleUrls: ['./enter-data.component.scss'],
 })
-export class EnterDataComponent implements OnDestroy {
+export class EnterDataComponent implements OnInit, OnDestroy {
   private subscriptions = new Subscription();
   public subAreaData;
 
   public onChildRoute = false;
-  public formParams = {
-    parkName: '',
-    subAreaName: '',
-    date: '',
-    orcs: '',
-    subAreaId: '',
-    isLegacy: false,
-  };
+  public urlParams;
   public utils = new Utils();
 
   public text = `Select the data and location above for the Attendance and Revenue data you
   want to enter. If you want to view past enteries, you can do that by selecting
   the date and location you want to view.`;
 
-  constructor(protected dataService: DataService, protected router: Router) {
+  constructor(protected dataService: DataService, protected router: Router, protected urlService: UrlService) {
     this.subscriptions.add(
       dataService
         .watchItem(Constants.dataIds.ENTER_DATA_SUB_AREA)
@@ -43,19 +38,17 @@ export class EnterDataComponent implements OnDestroy {
         .pipe(filter((event) => event instanceof NavigationEnd))
         .subscribe((event: any) => {
           this.onChildRoute =
-          event.url.split('?')[0] !== '/enter-data' ? true : false;
-        })
-        );
-
-    this.subscriptions.add(
-      dataService
-        .watchItem(Constants.dataIds.ENTER_DATA_URL_PARAMS)
-        .subscribe((res) => {
-          if (res) {
-            this.formParams = res;
-          }
+            event.url.split('?')[0] !== '/enter-data' ? true : false;
         })
     );
+  }
+
+  ngOnInit(): void {
+    this.urlParams = this.urlService.getQueryParams();
+  }
+
+  formatDate(date): string {
+    return DateTime.fromFormat(date, 'yyyyLL').toFormat('LLLL yyyy');
   }
 
   ngOnDestroy() {
