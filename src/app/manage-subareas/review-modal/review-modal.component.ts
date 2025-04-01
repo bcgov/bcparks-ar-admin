80f7b087cfad2f8b4ae13bba5c2ca644622b915e
Cameron Pettit <71421099+cameronpettit@users.noreply.github.com>
2025-04-01 15:32:15 -0700

    AR-427-4 sendback changes (#447)
    * AR-427-4 sendback changes
    
    * AR-427-4 fixing tests

--- src/app/manage-subareas/review-modal/review-modal.component.ts
+++ src/app/manage-subareas/review-modal/review-modal.component.ts
@@ -2,10 +2,10 @@ import { AfterViewInit, Component, EventEmitter, Input, Output, TemplateRef, Vie
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
-    selector: 'app-review-modal',
-    templateUrl: './review-modal.component.html',
-    styleUrls: ['./review-modal.component.scss'],
-    standalone: false
+  selector: 'app-review-modal',
+  templateUrl: './review-modal.component.html',
+  styleUrls: ['./review-modal.component.scss'],
+  standalone: false
 })
 export class ReviewModalComponent implements AfterViewInit {
   @ViewChild('reviewModal') reviewModal: TemplateRef<any>;
@@ -35,6 +35,35 @@ export class ReviewModalComponent implements AfterViewInit {
         this.decline.emit();
       }
     });
+    this.convertToSentenceCase();
+  }
+
+  // This function converts the value of the Activities field to sentence case
+  // It is here to satisfy the designs of the implementation ticket as the 
+  // designs display the activities as sentence case, but the API expects
+  // the activities to be in capital case, so we have to diverge here.
+  // https://github.com/bcgov/bcparks-ar-admin/issues/427
+  convertToSentenceCase() {
+    let activities = this.newData.find(item => item.label === 'Activities');
+    let convertedActivities = [];
+    let separatedActivities = activities?.value.split(', ');
+    if (separatedActivities && separatedActivities.length > 1) {
+      for (const activity of separatedActivities) {
+        let words = activity.split(' ') || activity;
+        if (words && words.length > 1) {
+          for (let i = 1; i <= words.length - 1; i++) {
+            words[i] = words[i]?.toLowerCase();
+          }
+        }
+        const convertedActivity = words.join(' ');
+        convertedActivities.push(convertedActivity);
+      }
+      this.newData.splice(this.newData.indexOf(activities), 1);
+      this.newData.push({
+        label: 'Activities',
+        value: convertedActivities.join(', ')
+      })
+    }
   }
 
   confirmClicked() {
